96f2104277f8ff34234eee0675d4e8f21a609ef5
peng19880925 <pengpyc@gmail.com>
2017-09-27 11:16:44 -0400

    Create kmeans.py

---
+++ sklearn/kmeans.py
@@ -0,0 +1,17 @@
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+
+#data clean: missing imputation and scale
+features = build_X.columns.tolist()
+build_X_clean = build_X.fillna(build_X.median())
+build_X_normalized = MinMaxScaler().fit(build_X_clean).transform(build_X_clean)  
+build_X_normalized = pd.DataFrame(data = build_X_normalized, columns = features)
+
+# build Kmean
+kmeans = KMeans(n_clusters = 10, random_state = 0)
+kmeans.fit(build_X_normalized)
+
+cluster = kmeans.predict(build_X_normalized)
+build_X['Cluster'] = cluster
+
+# Test the optimal cluster
